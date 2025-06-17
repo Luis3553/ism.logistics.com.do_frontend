9fa4388c3b1f42f8fcd6a2c027348074018c4b49
2190031 <115372942+2190031@users.noreply.github.com>
2025-06-17 09:48:49 -0400

    build mode base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+    base: mode === "production" ? "/frontend/" : "/",
     build: {
         sourcemap: true,
     },
-    base: "/",
     server: {
         allowedHosts: true,
     },
     plugins: [react()],
     resolve: {
         alias: {
-          '@hooks': path.resolve(__dirname, 'src/hooks'),
-          '@api': path.resolve(__dirname, 'src/api'),
-          '@components': path.resolve(__dirname, 'src/components'),
-          '@charts': path.resolve(__dirname, 'src/charts'),
-          '@pages': path.resolve(__dirname, 'src/pages'),
-          '@utils': path.resolve(__dirname, 'src/utils'),
-          '@assets': path.resolve(__dirname, 'src/assets'),
-          '@public': path.resolve(__dirname, 'public'),
-          // Add others as needed
-        }
-      }
-});
+            "@hooks": path.resolve(__dirname, "src/hooks"),
+            "@api": path.resolve(__dirname, "src/api"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@charts": path.resolve(__dirname, "src/charts"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@utils": path.resolve(__dirname, "src/utils"),
+            "@assets": path.resolve(__dirname, "src/assets"),
+            "@public": path.resolve(__dirname, "public"),
+            // Add others as needed
+        },
+    },
+}));
